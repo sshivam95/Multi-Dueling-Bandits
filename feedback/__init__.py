79c2ed35e4716af2d5a307114ffb91745c32aa2f
Shivam Sharma <sshivam95@gmail.com>
2022-10-10 00:41:26 +0200

    Remove unecessary stuff

--- feedback/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Various mechanisms for comparing arms."""
-from feedback.feedback_mechanism import FeedbackMechanism
-from feedback.matrix_feedback import MatrixFeedback
-
-__all__ = ["FeedbackMechanism", "MatrixFeedback"]
