ff8c8121b1650a3f3c4401e86689fb64ded0ea80
shivam <sshivam95@gmail.com>
2022-11-14 11:18:51 +0100

    Restore original colstim v2

--- algorithms/colstim.py
+++ algorithms/colstim.py
@@ -1,8 +1,9 @@
 import logging
 from time import perf_counter
 from typing import Optional
-from tqdm import tqdm
+
 import numpy as np
+from tqdm import tqdm
 
 from algorithms.algorithm import Algorithm
 from feedback.multi_duel_feedback import MultiDuelFeedback
@@ -84,7 +85,7 @@ class Colstim(Algorithm):
 
     def step(self):
         context_vector = self.context_matrix[self.time_step - 1]
-            
+
         self.sample_perturbation_variable()
         self.skill_vector[self.time_step - 1] = self.get_skill_vector(
             theta=self.theta_hat, context_vector=context_vector
@@ -113,10 +114,10 @@ class Colstim(Algorithm):
         self.preference_estimate.enter_sample(winner_arm=self.winner)
         self.logger.debug("Updating Theta...")
         self.update_estimated_theta(
-                selection=self.selection,
-                time_step=self.time_step,
-                winner=self.winner,
-            )
+            selection=self.selection,
+            time_step=self.time_step,
+            winner=self.winner,
+        )
         self.update_mean_theta(self.time_step)
         self.compute_regret(selection=self.selection, time_step=self.time_step)
 
@@ -181,9 +182,14 @@ class Colstim_v2(Colstim):
         end_time = perf_counter()
         self.execution_time = end_time - start_time
         print("Algorithm Finished...")
-        
+
     def step(self):
-        self.context_vector = self.context_matrix[self.time_step - 1]            
+        self.context_vector = self.context_matrix[self.time_step - 1]
+        if self.time_step > 1:
+            self.update_estimated_theta(
+                selection=self.selection, time_step=self.time_step, winner=self.winner
+            )
+            self.update_mean_theta(self.time_step)
         self.sample_perturbation_variable()
         self.skill_vector[self.time_step - 1] = self.get_skill_vector(
             theta=self.theta_hat, context_vector=self.context_vector
@@ -210,13 +216,6 @@ class Colstim_v2(Colstim):
         self.logger.debug("Duels finished...")
         self.logger.debug(f"    -> Selection Winner: {self.winner}")
         self.preference_estimate.enter_sample(winner_arm=self.winner)
-        self.logger.debug("Updating Theta...")
-        self.update_estimated_theta(
-            selection=self.selection,
-            time_step=self.time_step,
-            winner=self.winner
-        )
-        self.update_mean_theta(self.time_step)
         self.compute_regret(selection=self.selection, time_step=self.time_step)
 
     def get_selection_v2(self, quality_of_arms):
