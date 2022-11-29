2ffed4f759eb25c6c101c2ef18a4fac057afa611
sshivam95 <sshivam95@gmail.com>
2022-11-29 12:49:13 +0100

    Sort Noctua 1 and Noctua 2 results

--- test_cluster.py
+++ test_cluster.py
@@ -194,9 +194,9 @@ def run_experiment(
             mask = (result_df["algorithm"] == name) & (result_df["rep_id"] == rep_id)
             regrets[rep_id] = result_df[mask]["regret"]
             execution_times[rep_id] = result_df[mask]["execution_time"].mean()
-        np.save(f"Regret_results//regret_{name}_{solver}_{subset_size}.npy", regrets)
+        np.save(f"Noctua_2_results/Regret_results_theta_bar_correct_feedback_50_framework_v1/regret_{name}_{solver}_{subset_size}.npy", regrets)
         np.save(
-            f"Execution_times_results//execution_time_{name}_{solver}_{subset_size}.npy",
+            f"Noctua_2_results/Execution_results_theta_bar_correct_feedback_50_framework_v1/execution_time_{name}_{solver}_{subset_size}.npy",
             execution_times,
         )
     print(f"Experiments took {round(runtime)}s.")
