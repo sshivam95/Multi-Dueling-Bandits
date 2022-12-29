a201d18ccf83c0d36fbaed17a31184cc8b99cb6f
shivam <sshivam95@gmail.com>
2022-12-29 21:55:14 +0100

    Add Job Scripts

---
+++ Job_Scripts_Noctua2/script_parameter_setup_Colstim_v2_mips.sh
@@ -0,0 +1,18 @@
+declare -a num_arms=("20")
+declare -a algorithm_classes=("Colstim_v2")
+declare -a solver_array=("mips")
+declare -a subset_size=("16")
+
+for arms in "${num_arms[@]}";
+do
+  for algorithm in "${algorithm_classes[@]}";
+  do
+    for solver in "${solver_array[@]}";
+    do
+      for size in "${subset_size[@]}";
+      do
+        sbatch run_script_Colstim_v2_mips.sh $algorithm $solver $size $arms
+      done
+    done
+  done
+done
---
+++ Job_Scripts_Noctua2/script_parameter_setup_ts_iss.sh
@@ -0,0 +1,18 @@
+declare -a num_arms=("15")
+declare -a algorithm_classes=("ThompsonSampling" "ThompsonSamplingContextual" "IndependentSelfSparring" "IndependentSelfSparringContextual")
+declare -a solver_array=("saps" "mips")
+declare -a subset_size=("2" "3" "4" "5" "6" "7" "8" "9" "10")
+
+for arms in "${num_arms[@]}";
+do
+  for algorithm in "${algorithm_classes[@]}";
+  do
+    for solver in "${solver_array[@]}";
+    do
+      for size in "${subset_size[@]}";
+      do
+        sbatch run_script_ts_iss.sh $algorithm $solver $size $arms
+      done
+    done
+  done
+done
